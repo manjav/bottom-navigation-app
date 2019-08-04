7eeba4b6e97e9d620962ae0c128bc5bb0515bf34
Mansour <mansurjavadi@gmail.com>
2019-08-05 01:01:43 +0430

    expandable list added

---
+++ app/src/main/java/com/grantech/cinnagen/cinnora/ExpandableListDataPump.java
@@ -0,0 +1,15 @@
+package com.grantech.cinnagen.cinnora;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListDataPump {
+    public static HashMap<Integer, List<Integer>> getData() {
+        HashMap<Integer, List<Integer>> expandableListDetail = new HashMap<>();
+        expandableListDetail.put(R.string.title_navi_0, new ArrayList<Integer>(){{add(R.string.title_navi_0);}});
+        expandableListDetail.put(R.string.title_navi_1, new ArrayList<Integer>(){{add(R.string.title_navi_1);}});
+        expandableListDetail.put(R.string.title_navi_2, new ArrayList<Integer>(){{add(R.string.title_navi_2);}});
+        return expandableListDetail;
+    }
+}
--- app/src/main/java/com/grantech/cinnagen/cinnora/fragments/TipsFragment.java
+++ app/src/main/java/com/grantech/cinnagen/cinnora/fragments/TipsFragment.java
@@ -6,8 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
+import com.grantech.cinnagen.cinnora.ExpandableListAdapter;
+import com.grantech.cinnagen.cinnora.ExpandableListDataPump;
 import com.grantech.cinnagen.cinnora.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Belal on 1/23/2018.
@@ -15,13 +23,65 @@ import com.grantech.cinnagen.cinnora.R;
 
 public class TipsFragment extends Fragment
 {
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<Integer> expandableListTitle;
+    HashMap<Integer, List<Integer>> expandableListDetail;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
         return inflater.inflate(R.layout.tips_fragment_layout, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        expandableListView = view.findViewById(R.id.expandableListView);
+        expandableListDetail = ExpandableListDataPump.getData();
+        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+        expandableListAdapter = new ExpandableListAdapter(getContext().getApplicationContext(), expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition)
+            {
+                if( expandableListAdapter == null )
+                    return;
+                for( int i = 0; i < expandableListAdapter.getGroupCount(); i++ )
+                    if( i != groupPosition )
+                        expandableListView.collapseGroup(i);
+
+//              Toast.makeText(getContext().getApplicationContext(), expandableListTitle.get(groupPosition) + " List Expanded.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+/*        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition)
+            {
+              Toast.makeText(getContext().getApplicationContext(), expandableListTitle.get(groupPosition) + " List Collapsed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getContext().getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                )
+                        .show();
+                return false;
+            }
+        });*/
     }
 }
