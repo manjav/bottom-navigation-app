46c5f45d660d2626f656cb63ad443d5c4dd97621
Mansour <mansurjavadi@gmail.com>
2019-08-11 02:29:25 +0430

    added new fragments organizer back-button added into action-bar

--- app/src/main/java/com/grantech/cinnagen/solife/MainActivity.java
+++ app/src/main/java/com/grantech/cinnagen/solife/MainActivity.java
@@ -3,13 +3,11 @@ package com.grantech.cinnagen.solife;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,110 +15,105 @@ import com.grantech.cinnagen.solife.fragments.CalendarFragment;
 import com.grantech.cinnagen.solife.fragments.InjectionFragment;
 import com.grantech.cinnagen.solife.fragments.MoreFragment;
 import com.grantech.cinnagen.solife.fragments.TipsFragment;
+import com.grantech.cinnagen.solife.utils.Fragments;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
 {
-    private Locale locale;
-    private int startingPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         // default localization is farsi
-        locale = new Locale("fa");
-        Locale.setDefault(locale);
+        Fragments.getInstance().locale = new Locale("fa");
+        Locale.setDefault(Fragments.getInstance().locale);
         Configuration config = new Configuration();
-        config.locale = locale;
+        config.locale = Fragments.getInstance().locale;
         Resources res = getApplicationContext().getResources();
         res.updateConfiguration(config, res.getDisplayMetrics());
 
         // custom action-bar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_medication_settings);
 
         super.onCreate(savedInstanceState);
 
 //         custom fonts
 //        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.O )
-//            FontsOverride.setFont("MONOSPACE", getResources().getFont(R.font.iransansmobile_bold));
+//            FontsOverride.setFont("normal", getResources().getFont(R.font.iransansmobile_light));
 
         setContentView(R.layout.activity_main);
-        loadFragment(new InjectionFragment(), 0, R.string.title_navi_0);
+        Fragments.getInstance().loadFragment(this, new InjectionFragment(), 0, R.string.title_navi_0);
         ((BottomNavigationView) findViewById(R.id.navigation)).setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private boolean loadFragment(Fragment fragment, int newPosition, int title)
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
+        Fragment fragment = null;
+        int fragmentTitle = 0;
+        int newPosition = 0;
+
+        switch (item.getItemId())
+        {
+            case R.id.navi_0:
+                newPosition = 0;
+                fragmentTitle = R.string.title_navi_0;
+                fragment = new InjectionFragment();
+                break;
+
+            case R.id.navi_1:
+                newPosition = 1;
+                fragmentTitle = R.string.title_navi_1;
+                fragment = new CalendarFragment();
+                break;
+
+            case R.id.navi_2:
+                newPosition = 2;
+                fragmentTitle = R.string.title_navi_2;
+                fragment = new TipsFragment();
+                break;
+
+            case R.id.navi_3:
+                newPosition = 3;
+                fragmentTitle = R.string.title_navi_3;
+                fragment = new MoreFragment();
+                break;
+        }
+
+        return Fragments.getInstance().loadFragment(this, fragment, newPosition, fragmentTitle);
+    };
+
+    @Override
+    public void onBackPressed()
     {
-        if( fragment == null )
-            return false;
-
-        // switching fragment
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        if( startingPosition > -1 )
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START))
         {
-            if( startingPosition > newPosition )
-                transaction.setCustomAnimations(locale.getLanguage().equals("fa") ? R.anim.slide_in_right : R.anim.slide_in_left, locale.getLanguage().equals("fa") ? R.anim.slide_out_left : R.anim.slide_out_right );
-            else
-                transaction.setCustomAnimations(locale.getLanguage().equals("fa") ? R.anim.slide_in_left : R.anim.slide_in_right, locale.getLanguage().equals("fa") ? R.anim.slide_out_right : R.anim.slide_out_left);
-        }
+            drawer.closeDrawer(GravityCompat.START);
+            return;
+        }*/
 
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.commit();
-        startingPosition = newPosition;
-
-        // hide action-bar in main page
-        if( newPosition != 0 )
+        int stackCount = fragmentManager.getBackStackEntryCount();
+        if( stackCount > 0 )
         {
-            getSupportActionBar().show();
-            ((AppCompatTextView) findViewById(R.id.action_bar_text_view)).setText(title);
-        }
-        else
-        {
-            getSupportActionBar().hide();
-        }
+//            transaction.setCustomAnimations(Fragments.getInstance().getAnimationIn(true), Fragments.getInstance().getAnimationOut(true));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(stackCount > 1);
+            fragmentManager.popBackStack();
 
-        return true;
+            transaction.commit();
+            return;
+        }
+        super.onBackPressed();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener()
+    @Override
+    public boolean onSupportNavigateUp()
     {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item)
-        {
-            Fragment fragment = null;
-            int fragmentTitle = 0;
-            int newPosition = 0;
-
-            switch (item.getItemId())
-            {
-                case R.id.navi_0:
-                    newPosition = 0;
-                    fragmentTitle = R.string.title_navi_0;
-                    fragment = new InjectionFragment();
-                    break;
-
-                case R.id.navi_1:
-                    newPosition = 1;
-                    fragmentTitle = R.string.title_navi_1;
-                    fragment = new CalendarFragment();
-                    break;
-
-                case R.id.navi_2:
-                    newPosition = 2;
-                    fragmentTitle = R.string.title_navi_2;
-                    fragment = new TipsFragment();
-                    break;
-
-                case R.id.navi_3:
-                    newPosition = 3;
-                    fragmentTitle = R.string.title_navi_3;
-                    fragment = new MoreFragment();
-                    break;
-            }
-
-            return loadFragment(fragment, newPosition, fragmentTitle);
-        }
-    };
+        onBackPressed();
+        return true;
+    }
 }
+
---
+++ app/src/main/java/com/grantech/cinnagen/solife/utils/Fragments.java
@@ -0,0 +1,77 @@
+package com.grantech.cinnagen.solife.utils;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.grantech.cinnagen.solife.R;
+
+import java.util.Locale;
+
+public class Fragments
+{
+    private static final Fragments ourInstance = new Fragments();
+
+    public Locale locale;
+    private int oldPosition = -1;
+    static public Fragments getInstance() {
+        return ourInstance;
+    }
+
+    private Fragments() {
+
+    }
+
+
+    public boolean loadFragment(AppCompatActivity activity, Fragment fragment, int newPosition, int title)
+    {
+        if( fragment == null )
+            return false;
+
+        // switching fragment
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        if( oldPosition > -1 )
+        {
+            if( newPosition > 3 )
+                transaction.setCustomAnimations(getAnimationIn(oldPosition > newPosition), getAnimationOut(oldPosition > newPosition), getAnimationIn(oldPosition < newPosition), getAnimationOut(oldPosition < newPosition));
+            transaction.setCustomAnimations(getAnimationIn(oldPosition > newPosition), getAnimationOut(oldPosition > newPosition));
+        }
+
+        transaction.replace(R.id.fragment_container, fragment);
+        if( newPosition > 3 )
+            transaction.addToBackStack("tag");
+        transaction.commit();
+        oldPosition = newPosition;
+
+        // hide action-bar in main page
+        ActionBar actionBar = activity.getSupportActionBar();
+        if( newPosition != 0 )
+        {
+            actionBar.show();
+            actionBar.setDisplayHomeAsUpEnabled(newPosition > 3);
+//            actionBar.setDisplayShowHomeEnabled(newPosition > 3);
+            ((AppCompatTextView) activity.findViewById(R.id.action_bar_text_view)).setText(title);
+        }
+        else
+        {
+            actionBar.hide();
+        }
+
+        return true;
+    }
+
+    public int getAnimationIn(boolean isBack)
+    {
+        if( isBack )
+            return locale.getLanguage().equals("fa") ? R.anim.slide_in_right : R.anim.slide_in_left;
+        return locale.getLanguage().equals("fa") ? R.anim.slide_in_left : R.anim.slide_in_right;
+    }
+    public int getAnimationOut(boolean isBack)
+    {
+        if( isBack )
+            return locale.getLanguage().equals("fa") ? R.anim.slide_out_left : R.anim.slide_out_right;
+        return locale.getLanguage().equals("fa") ? R.anim.slide_out_right : R.anim.slide_out_left;
+    }
+}
