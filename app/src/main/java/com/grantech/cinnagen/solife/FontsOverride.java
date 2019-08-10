a695644a95543b876b6ea714e27418bc875eff5d
Mansour <mansurjavadi@gmail.com>
2019-08-10 10:37:14 +0430

    cleaned up

--- app/src/main/java/com/grantech/cinnagen/solife/FontsOverride.java
+++ app/src/main/java/com/grantech/cinnagen/solife/FontsOverride.java
@@ -2,26 +2,31 @@ package com.grantech.cinnagen.solife;
 
 import android.content.Context;
 import android.graphics.Typeface;
+
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by vamsi on 06-05-2017 for Android custom font article
  */
 public class FontsOverride
 {
-    public static void setDefaultFont(Context context, String staticTypefaceFieldName, String fontAssetName)
+    static Map<String, Typeface> map = new HashMap<>();
+    public static void setFont(Context context, String defaultFontNameToOverride, String fontAssetName)
     {
-        final Typeface regular = Typeface.createFromAsset(context.getAssets(), fontAssetName);
-        replaceFont(staticTypefaceFieldName, regular);
+        final Typeface customFontTypeface = Typeface.createFromAsset(context.getAssets(), fontAssetName);
+        try {
+            final Field defaultFontTypefaceField = Typeface.class.getDeclaredField(defaultFontNameToOverride);
+            map.put(fontAssetName, customFontTypeface);
+            defaultFontTypefaceField.setAccessible(true);
+            defaultFontTypefaceField.set(null, customFontTypeface);
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
-    protected static void replaceFont(String staticTypefaceFieldName, final Typeface newTypeface)
+    public static Typeface getTypeface(String name)
     {
-        try {
-            final Field staticField = Typeface.class.getDeclaredField(staticTypefaceFieldName);
-            staticField.setAccessible(true);
-            staticField.set(null, newTypeface);
-        } catch (NoSuchFieldException e) { e.printStackTrace();
-        } catch (IllegalAccessException e) { e.printStackTrace(); }
+        return map.get(name);
     }
+
 }
