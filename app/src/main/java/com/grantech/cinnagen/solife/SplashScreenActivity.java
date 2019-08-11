866e9637e560f2b89e1cef9352dc6fe217cd7a8b
Mansour <mansurjavadi@gmail.com>
2019-08-11 17:35:52 +0430

    splash-screen added

---
+++ app/src/main/java/com/grantech/cinnagen/solife/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.grantech.cinnagen.solife;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class SplashScreenActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
