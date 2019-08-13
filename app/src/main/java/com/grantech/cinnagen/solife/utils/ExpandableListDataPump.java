d0bf806409bcc45d6c19a7c230a26cca2be226e3
Mansour <mansurjavadi@gmail.com>
2019-08-13 12:04:20 +0430

    cleaned up

--- app/src/main/java/com/grantech/cinnagen/solife/utils/ExpandableListDataPump.java
+++ app/src/main/java/com/grantech/cinnagen/solife/utils/ExpandableListDataPump.java
@@ -11,9 +11,9 @@ public class ExpandableListDataPump
     public static HashMap<Integer, List<Integer>> getData()
     {
         HashMap<Integer, List<Integer>> expandableListDetail = new HashMap<>();
-        expandableListDetail.put(R.string.title_navi_0, new ArrayList<Integer>(){{add(R.string.title_navi_0);}});
-        expandableListDetail.put(R.string.title_navi_1, new ArrayList<Integer>(){{add(R.string.title_navi_1);}});
-        expandableListDetail.put(R.string.title_navi_2, new ArrayList<Integer>(){{add(R.string.title_navi_2);}});
+        expandableListDetail.put(R.string.home_injection, new ArrayList<Integer>(){{add(R.string.home_injection);}});
+        expandableListDetail.put(R.string.home_calendar, new ArrayList<Integer>(){{add(R.string.home_calendar);}});
+        expandableListDetail.put(R.string.home_tips, new ArrayList<Integer>(){{add(R.string.home_tips);}});
         return expandableListDetail;
     }
 }
