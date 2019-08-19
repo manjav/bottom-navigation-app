54c03b142792398e62fc654c42db4f8b49ed4289
Mansour <mansurjavadi@gmail.com>
2019-08-19 10:24:36 +0430

    added new injection activity added expandable-docs-fragment

---
+++ app/src/main/java/com/grantech/cinnagen/solife/activities/InjectionActivity.java
@@ -0,0 +1,46 @@
+package com.grantech.cinnagen.solife.activities;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.grantech.cinnagen.solife.R;
+import com.grantech.cinnagen.solife.utils.Fragments;
+
+import java.util.Objects;
+
+public class InjectionActivity extends BaseActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_injection);
+        Fragments.getInstance().loadFragment(this, R.dimen.position_injection_start);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        int stackCount = fragmentManager.getBackStackEntryCount();
+        if( stackCount == 1 )
+        {
+            Fragments.getInstance().clearStack(this);
+            finish();
+            return;
+        }
+        if( stackCount > 1 )
+        {
+            String lastFragmentName = fragmentManager.getBackStackEntryAt(stackCount - 1).getName();
+            Fragments.getInstance().oldPosition = Integer.parseInt(lastFragmentName);
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+            transaction.commit();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+}
---
+++ app/src/main/java/com/grantech/cinnagen/solife/fragments/DocsExpandableFragment.java
@@ -0,0 +1,46 @@
+package com.grantech.cinnagen.solife.fragments;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.grantech.cinnagen.solife.R;
+
+/**
+ * Created by ManJav on 1/23/2019.
+ */
+
+public class DocsExpandableFragment extends BaseFragment
+{
+    @SuppressLint("InflateParams")
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_docs_expandable, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        WebView webView = view.findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(false);
+        settings.setDomStorageEnabled(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        assert getArguments() != null;
+        webView.loadUrl(getArguments().getString("url"));
+
+
+    }
+}
