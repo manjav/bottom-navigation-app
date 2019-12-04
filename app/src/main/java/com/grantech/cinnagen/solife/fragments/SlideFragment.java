06b2e96e259c8b83c301eb5c1a74673952db5a58
Mansour <mansurjavadi@gmail.com>
2019-12-04 17:58:24 +0330

    slide captions added

--- app/src/main/java/com/grantech/cinnagen/solife/fragments/SlideFragment.java
+++ app/src/main/java/com/grantech/cinnagen/solife/fragments/SlideFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,8 +23,9 @@ import java.util.Objects;
 public class SlideFragment extends Fragment
 {
     private final int position;
+    private TextView slideText;
     private ImageView slideImage;
-
+    private int[] captions = new int[]{R.string.injection_slides_0, R.string.injection_slides_1, R.string.injection_slides_2, R.string.injection_slides_3, R.string.injection_slides_4};
     SlideFragment(int position)
     {
         this.position = position;
@@ -41,6 +43,7 @@ public class SlideFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
         slideImage = view.findViewById(R.id.slide_image);
+        slideText = view.findViewById(R.id.slide_text);
     }
 
     @Override
@@ -49,5 +52,6 @@ public class SlideFragment extends Fragment
         super.onResume();
         int id = getResources().getIdentifier("slide_" + position, "mipmap", Objects.requireNonNull(getContext()).getPackageName());
         slideImage.setImageResource(id);
+        slideText.setText(captions[captions.length - position - 1]);
     }
 }
