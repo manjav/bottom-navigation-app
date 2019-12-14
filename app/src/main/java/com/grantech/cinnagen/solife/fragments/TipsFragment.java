3f2194e59181a6e6f569b8fe59bf25822cd4aee7
Mansour <mansurjavadi@gmail.com>
2019-12-14 11:32:02 +0330

    tips rearranged

--- app/src/main/java/com/grantech/cinnagen/solife/fragments/TipsFragment.java
+++ app/src/main/java/com/grantech/cinnagen/solife/fragments/TipsFragment.java
@@ -39,14 +39,14 @@ public class TipsFragment extends BaseFragment
 
         expandableListAdapter = new ExpandableListAdapter(Objects.requireNonNull(getContext()).getApplicationContext());
         expandableListAdapter.addChild(R.string.tips_0_title, new ArrayList<Integer>(){{add(R.string.tips_0_message);}});
-        expandableListAdapter.addChild(R.string.tips_1_title, new ArrayList<Integer>(){{add(R.string.tips_1_message_0);add(R.string.tips_1_message_1);add(R.string.tips_1_message_2);add(R.string.tips_1_message_3);}});
-        expandableListAdapter.addChild(R.string.tips_2_title, new ArrayList<Integer>(){{add(R.string.tips_2_message);}});
-        expandableListAdapter.addChild(R.string.tips_3_title, new ArrayList<Integer>(){{add(R.string.tips_3_message);}});
         expandableListAdapter.addChild(R.string.tips_4_title, new ArrayList<Integer>(){{add(R.string.tips_4_message);}});
-        expandableListAdapter.addChild(R.string.tips_5_title, new ArrayList<Integer>(){{add(R.string.tips_5_message);}});
-        expandableListAdapter.addChild(R.string.tips_6_title, new ArrayList<Integer>(){{add(R.string.tips_6_message);}});
         expandableListAdapter.addChild(R.string.tips_7_title, new ArrayList<Integer>(){{add(R.string.tips_7_message);}});
         expandableListAdapter.addChild(R.string.tips_8_title, new ArrayList<Integer>(){{add(R.string.tips_8_message);}});
+        expandableListAdapter.addChild(R.string.tips_1_title, new ArrayList<Integer>(){{add(R.string.tips_1_message_0);add(R.string.tips_1_message_1);add(R.string.tips_1_message_2);add(R.string.tips_1_message_3);add(R.string.tips_1_message_4);}});
+        expandableListAdapter.addChild(R.string.tips_2_title, new ArrayList<Integer>(){{add(R.string.tips_2_message);}});
+        expandableListAdapter.addChild(R.string.tips_5_title, new ArrayList<Integer>(){{add(R.string.tips_5_message);}});
+        expandableListAdapter.addChild(R.string.tips_6_title, new ArrayList<Integer>(){{add(R.string.tips_6_message);}});
+        expandableListAdapter.addChild(R.string.tips_3_title, new ArrayList<Integer>(){{add(R.string.tips_3_message);}});
 
         expandableListView = view.findViewById(R.id.expandableListView);
         expandableListView.setAdapter(expandableListAdapter);
