960cf0ff70f07875815d896b557dab51e8c1928e
Mansour <mansurjavadi@gmail.com>
2020-04-06 20:49:00 +0430

    notification sending bug for new android fixed

--- app/src/main/java/com/grantech/cinnagen/solife/utils/AlarmReceiver.java
+++ app/src/main/java/com/grantech/cinnagen/solife/utils/AlarmReceiver.java
@@ -1,12 +1,17 @@
 package com.grantech.cinnagen.solife.utils;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -15,6 +20,8 @@ import com.grantech.cinnagen.solife.activities.SplashScreenActivity;
 
 public class AlarmReceiver extends BroadcastReceiver
 {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
     public void onReceive(Context context, Intent intent)
     {
         Bundle bundle = intent.getExtras();
@@ -30,13 +37,24 @@ public class AlarmReceiver extends BroadcastReceiver
         notify(context, notiID, ticker, title, text, info, data, icon, sound);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static void notify(Context context, int id, String ticker, String title, String text, String info, String data, String icon, String sound)
     {
         Intent intent = new Intent(context, SplashScreenActivity.class);
         intent.putExtra("data", data);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, id + "")
+        NotificationManager notificationManager =(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationChannel notificationChannel = new NotificationChannel("solife-injection", "Injection Channel", NotificationManager.IMPORTANCE_HIGH);
+        notificationChannel.setShowBadge(true);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.BLUE);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setVibrationPattern(new long[]{100, 350, 500});
+        notificationManager.createNotificationChannel(notificationChannel);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, notificationChannel.getId())
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
                 .setSmallIcon(R.mipmap.ic_launcher_mono)
                 .setContentTitle(title)
